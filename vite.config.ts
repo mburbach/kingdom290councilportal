9e70d68912873ba09bd96e27047932d54fb4c887
mburbach <martin.burbach@comramo.de>
2026-03-12 23:58:13 +0100

    fix: build wiki page

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,13 @@ export default defineConfig({
   },
   build: {
     outDir: '../dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        wiki: resolve(__dirname, 'src/wiki.html')
+      }
+    }
   },
   server: {
     port: 5173,
